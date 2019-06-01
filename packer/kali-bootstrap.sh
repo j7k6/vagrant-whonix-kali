b8a2643fd6e119100e956c0888a0a11e06ddd810
j7k6 <mail@j7k6.org>
2019-06-01 15:28:48 +0200

    test

--- packer/kali-bootstrap.sh
+++ packer/kali-bootstrap.sh
@@ -12,3 +12,4 @@ mkdir -p /home/vagrant/.ssh
 curl -fsSL https://raw.github.com/hashicorp/vagrant/master/keys/vagrant.pub > /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
 systemctl enable --now ssh
+nmcli connection modify 'Wired connection 1' ipv4.method manual ipv4.addresses 10.152.152.11/18 ipv4.gateway 10.152.152.10 ipv4.dns 10.152.152.10
