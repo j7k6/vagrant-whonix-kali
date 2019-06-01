aee2e7c1a75288a2f5ca6852835af707c4745581
j7k6 <mail@j7k6.org>
2019-06-01 15:26:32 +0200

    test

--- provision.sh
+++ provision.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+export DEBIAN_FRONTEND="noninteractive"
+
 sudo rm /etc/ssh/ssh_host_*
 sudo dpkg-reconfigure openssh-server
 sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
